d12a13216329899ddb03f9f23de949e16a0f2968
aoml3245 <aoml3245@naver.com>
2024-08-21 16:33:27 +0900

    [fix] 자동 로그인 로직, 헤더 수정

--- src/utils/useAuth.ts
+++ src/utils/useAuth.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
-import api from "./api"; // axios 인스턴스
+import api from "./api";
 
 const useAuth = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -9,40 +9,27 @@ const useAuth = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const token = localStorage.getItem("accessToken");
+    const token = localStorage.getItem("token");
     if (token) {
       try {
         const decodedToken = jwtDecode(token) as { exp: number };
         const currentTime = Date.now() / 1000;
 
         if (decodedToken.exp > currentTime) {
+          // 토큰이 유효할 때만 navigate 호출
           setIsAuthenticated(true);
           api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-        } else {
-          // 토큰이 만료된 경우, refreshToken으로 새 accessToken을 요청
-          const refreshToken = localStorage.getItem("refreshToken");
-          if (refreshToken) {
-            api
-              .post("/auth/refresh", { token: refreshToken })
-              .then((response) => {
-                localStorage.setItem("accessToken", response.data.accessToken);
-                api.defaults.headers.common[
-                  "Authorization"
-                ] = `Bearer ${response.data.accessToken}`;
-                setIsAuthenticated(true);
-              })
-              .catch(() => {
-                localStorage.removeItem("accessToken");
-                localStorage.removeItem("refreshToken");
-                navigate("/login");
-              });
-          } else {
-            navigate("/login");
+          if (window.location.pathname === "/login") {
+            navigate("/portfolio-upload-text"); // 로그인 페이지에 있을 때만 리디렉션
           }
+        } else {
+          localStorage.removeItem("token");
+          localStorage.removeItem("refreshToken");
+          navigate("/login");
         }
       } catch (error) {
         console.error("토큰 디코딩 중 오류 발생:", error);
-        localStorage.removeItem("accessToken");
+        localStorage.removeItem("token");
         localStorage.removeItem("refreshToken");
         navigate("/login");
       }
@@ -53,7 +40,7 @@ const useAuth = () => {
   }, [navigate]);
 
   const logout = () => {
-    localStorage.removeItem("accessToken");
+    localStorage.removeItem("token");
     localStorage.removeItem("refreshToken");
     setIsAuthenticated(false);
     navigate("/login");
